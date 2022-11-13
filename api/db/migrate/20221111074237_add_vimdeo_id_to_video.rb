a1300688802f8b3efea8cd5ca2616a704531f054
yasumaeda3 <yasuhiromaeda@g.ecc.u-tokyo.ac.jp>
2022-11-13 10:15:55 +0900

    updated video schema

---
+++ api/db/migrate/20221111074237_add_vimdeo_id_to_video.rb
@@ -0,0 +1,6 @@
+class AddVimdeoIdToVideo < ActiveRecord::Migration[7.0]
+  def change
+    add_column(:videos, :vimeo_id, :string)
+    #Ex:- add_column("admin_users", "username", :string, :limit =>25, :after => "email")
+  end
+end
