34bff165397ac8571401c8f7fc92bc6a50348f1e
yasumaeda3 <yasuhiromaeda@g.ecc.u-tokyo.ac.jp>
2022-11-13 10:16:15 +0900

    added 1 seed

--- api/db/seeds.rb
+++ api/db/seeds.rb
@@ -7,5 +7,8 @@
 #   Character.create(name: "Luke", movie: movies.first)
 
 Video.create!(
-  
+  video_url:'https://vimeo.com/751086853', 
+  vimeo_id:'751086853',
+  length: 00:00:38,
+  thumbnail_url: 'https://madaminuv2-thumbnail-images.s3.ap-northeast-1.amazonaws.com/thumbnail_beppu_1.jpg'
 )
